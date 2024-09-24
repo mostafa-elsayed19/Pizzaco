d0fede43bab0d2ecef811d56c0c21c9f9a2dd54f
Mostafa <mostafaqaraman@outlook.com>
2024-09-24 16:46:25 +0300

    Added loader to menu component

--- src/features/menu/Menu.tsx
+++ src/features/menu/Menu.tsx
@@ -1,11 +1,15 @@
 import { useDataFetch } from "../../hooks/useDataFetch";
 import { getMenu } from "../../services/apiRestaurant";
 import Container from "../../ui/Container";
+import Loader from "../../ui/Loader";
 import MenuItem from "./MenuItem";
 
 function Menu() {
   const { data: menu, loading } = useDataFetch(getMenu);
   console.log(menu, loading);
+
+  if (loading) return <Loader />;
+
   return (
     <Container>
       <ul className="mx-auto flex flex-col divide-y-2 md:w-3/4">
