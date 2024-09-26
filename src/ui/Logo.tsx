a1e1f355a2d47e861ca67d3ac52aff004ce0e83c
Mostafa <mostafaqaraman@outlook.com>
2024-09-26 03:59:32 +0300

    Solved a small bug in logo

--- src/ui/Logo.tsx
+++ src/ui/Logo.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 function Logo({ height = "h-16" }) {
   return (
     <Link to="/">
-      <img src="logo.png" className={`${height}`} alt="" />
+      <img src="/logo.png" className={`${height}`} alt="" />
     </Link>
   );
 }
