725f0a698775aa65cb79794449a7b2d9deda7838
Mostafa <mostafaqaraman@outlook.com>
2024-09-26 18:36:01 +0300

    Added extra prop className for better reusability in container component

--- src/ui/Container.tsx
+++ src/ui/Container.tsx
@@ -5,15 +5,17 @@ function Container({
   display,
   padding = "py-4",
   textSize = "text-lg",
+  className,
 }: {
   children: ReactNode;
   display?: string;
   padding?: string;
   textSize?: string;
+  className?: string;
 }) {
   return (
     <div
-      className={`container mx-auto px-6 xl:px-0 ${display || ""} ${padding} ${textSize}`}
+      className={`container mx-auto px-6 xl:px-0 ${display || ""} ${padding} ${textSize} ${className}`}
     >
       {children}
     </div>
