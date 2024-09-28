e6058e975559f7e118d7f17e3edbf99901df19f3
Mostafa <mostafaqaraman@outlook.com>
2024-09-28 22:55:58 +0300

    Finished SearchInput to handle sending query to path

--- src/features/order/SearchOrder.tsx
+++ src/features/order/SearchOrder.tsx
@@ -1,12 +1,28 @@
+import { useNavigate } from "react-router-dom";
 import Input from "../../ui/Input";
+import { useState } from "react";
 
 function SearchOrder() {
+  const navigate = useNavigate();
+  const [query, setQuery] = useState("");
+
+  function handleSubmit(e: { preventDefault: () => void }) {
+    e.preventDefault();
+    if (!query) return;
+
+    navigate(`/order/${query}`);
+    setQuery("");
+  }
   return (
-    <Input
-      type="search"
-      placeholder="Search order #"
-      className="w-28 bg-background-color placeholder:text-slate-400 focus:w-48 md:w-48 md:focus:w-64 lg:w-64 lg:focus:w-72"
-    />
+    <form onSubmit={handleSubmit}>
+      <Input
+        type="search"
+        placeholder="Search order #"
+        className="w-28 bg-background-color placeholder:text-slate-400 focus:w-48 md:w-48 md:focus:w-64 lg:w-64 lg:focus:w-72"
+        onChange={(e) => setQuery(e.target.value)}
+        value={query}
+      />
+    </form>
   );
 }
 
