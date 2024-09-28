3c6e8056f033e7891d69fea1d6fbed7acea96867
Mostafa <mostafaqaraman@outlook.com>
2024-09-28 22:55:21 +0300

    Modified Input component to accept value and onChange props

--- src/ui/Input.tsx
+++ src/ui/Input.tsx
@@ -1,3 +1,5 @@
+import { ChangeEvent } from "react";
+
 function Input({
   id,
   type,
@@ -6,6 +8,8 @@ function Input({
   name,
   required,
   defaultValue,
+  onChange,
+  value,
 }: {
   id?: string;
   type: string;
@@ -14,16 +18,20 @@ function Input({
   name?: string;
   required?: boolean;
   defaultValue?: string | number;
+  onChange?: (e: ChangeEvent<HTMLInputElement>) => void;
+  value?: string;
 }) {
   return (
     <input
       id={id}
       type={type}
+      value={value}
       className={`input ${className}`}
       placeholder={placeholder}
       name={name}
       required={required}
       defaultValue={defaultValue}
+      onChange={onChange}
     />
   );
 }
