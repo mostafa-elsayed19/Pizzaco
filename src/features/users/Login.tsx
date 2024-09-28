b03ac4e398ba61e78bff0187fbfd3525c456d323
Mostafa <mostafaqaraman@outlook.com>
2024-09-28 22:38:04 +0300

    Created the Login form

--- src/features/users/Login.tsx
+++ src/features/users/Login.tsx
@@ -1,10 +1,27 @@
+import { Form } from "react-router-dom";
+import Input from "./../../ui/Input";
+import Button from "./../../ui/Button";
+import Logo from "../../ui/Logo";
+
 function Login() {
   return (
-    <div>
-      <p>login</p>
-      <p>login</p>
-      <p>login</p>
-      <p>login</p>
+    <div className="absolute inset-0 flex flex-col items-center justify-center gap-5 bg-background-color">
+      <Logo link={false} height="h-48" />
+      <Form className="flex flex-col gap-4">
+        <Input
+          required={true}
+          name="username"
+          placeholder="Username"
+          type="text"
+        />
+        <Input
+          required={true}
+          name="password"
+          placeholder="Password"
+          type="password"
+        />
+        <Button type="primary">Login</Button>
+      </Form>
     </div>
   );
 }
