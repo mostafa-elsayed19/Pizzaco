7d6826c41c6dd7bd2be8769a00963bff41be2b51
Mostafa <mostafaqaraman@outlook.com>
2024-09-28 23:01:16 +0300

    Modified text calar in search

--- src/features/order/SearchOrder.tsx
+++ src/features/order/SearchOrder.tsx
@@ -18,7 +18,7 @@ function SearchOrder() {
       <Input
         type="search"
         placeholder="Search order #"
-        className="w-28 bg-background-color placeholder:text-slate-400 focus:w-48 md:w-48 md:focus:w-64 lg:w-64 lg:focus:w-72"
+        className="w-28 bg-background-color text-text-color placeholder:text-slate-400 focus:w-48 md:w-48 md:focus:w-64 lg:w-64 lg:focus:w-72"
         onChange={(e) => setQuery(e.target.value)}
         value={query}
       />
