0d67004b77de8e9ef48a1cfd6456ed13bcb63915
Mostafa <mostafaqaraman@outlook.com>
2024-09-28 23:25:14 +0300

    Modified getMenu api error handling and modified ErrorPage accordingly

--- src/services/apiRestaurant.ts
+++ src/services/apiRestaurant.ts
@@ -3,7 +3,7 @@ import { supabase } from "./supabase";
 export async function getMenu() {
   const { data: menu, error } = await supabase.from("menu").select("*");
 
-  if (error) throw new Error(`Issue fetching data, ${error.message}`);
+  if (error) throw new Error(`Failed getting menu`);
 
   return menu;
 }
--- src/ui/ErrorPage.tsx
+++ src/ui/ErrorPage.tsx
@@ -3,11 +3,15 @@ import Container from "./Container";
 
 function ErrorPage() {
   const error = useRouteError();
-  console.log(error);
+
   return (
     <main className="bg-background-color text-text-color">
       <Container display="flex flex-col items-center justify-center gap-4 h-screen">
-        <p className="text-2xl">Something went wrong</p>
+        <p className="text-2xl">
+          {error instanceof Error
+            ? `${error?.message}`
+            : "Something went wrong"}
+        </p>
         <Link to="/" className="w-fit rounded-md bg-main-color px-4 py-2">
           &larr; take me home
         </Link>
