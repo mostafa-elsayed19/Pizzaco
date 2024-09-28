d49141921d14ddb9d9357c5ef09b399bcfa538fa
Mostafa <mostafaqaraman@outlook.com>
2024-09-28 23:05:53 +0300

    Prepared location Api

---
+++ src/services/apiLocation.ts
@@ -0,0 +1,17 @@
+export async function getAddress({
+  latitude,
+  longitude,
+}: {
+  latitude: string;
+  longitude: string;
+}) {
+  const res = await fetch(
+    `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${latitude}&longitude=${longitude}`,
+  );
+
+  if (!res.ok) throw Error("Failed getting address");
+
+  const data = await res.json();
+  console.log(data);
+  return data;
+}
