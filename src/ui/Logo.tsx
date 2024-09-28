93340ab303f808a32ae0128599588ecacf5dcce3
Mostafa <mostafaqaraman@outlook.com>
2024-09-28 22:37:43 +0300

    Logo is reusable and can be a link or a simple img

--- src/ui/Logo.tsx
+++ src/ui/Logo.tsx
@@ -1,6 +1,13 @@
 import { Link } from "react-router-dom";
 
-function Logo({ height = "h-16" }) {
+function Logo({
+  link = true,
+  height = "h-16",
+}: {
+  link?: boolean;
+  height?: string;
+}) {
+  if (!link) return <img src="/logo.png" className={`${height}`} alt="" />;
   return (
     <Link to="/">
       <img src="/logo.png" className={`${height}`} alt="" />
