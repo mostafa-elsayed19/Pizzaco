51d78d6c3abf6d311b86f7c0f59949037a190cea
Mostafa <mostafaqaraman@outlook.com>
2024-09-30 20:57:31 +0300

    Added more functions that will be needed in the future

--- src/features/cart/cartSlice.ts
+++ src/features/cart/cartSlice.ts
@@ -53,5 +53,11 @@ export default cartSlice.reducer;
 
 export const getCart = (state: RootState) => state.cart.cart;
 
+export const getTotalCartQuantity = (state: RootState) =>
+  state.cart.cart.reduce((sum, item) => sum + item.quantity, 0);
+
+export const getTotalCartPrice = (state: RootState) =>
+  state.cart.cart.reduce((sum, item) => sum + item.totalPrice, 0);
+
 export const getCurrentQuantityById = (id: number) => (state: RootState) =>
   state.cart.cart.find((item) => item.pizzaId === id)?.quantity ?? 0;
