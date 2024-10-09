dcc5cb9607ee1b2973a9a854890bf72c93171629
Mostafa <mostafaqaraman@outlook.com>
2024-10-09 06:13:27 +0300

    Fixed a small bug in Home page, now it shows user name if user is already provided before

--- src/features/users/CreateUser.tsx
+++ src/features/users/CreateUser.tsx
@@ -1,11 +1,12 @@
-import { useAppDispatch } from "../../hooks/useReduxMethods";
-import { updateName } from "./UserSlice";
+import { useAppDispatch, useAppSelector } from "../../hooks/useReduxMethods";
+import { getUserName, updateName } from "./UserSlice";
 import Input from "../../ui/Input";
 import Button from "../../ui/Button";
 import { FormEvent, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 function CreateUser() {
+  const user = useAppSelector(getUserName);
   const dispatch = useAppDispatch();
   const navigate = useNavigate();
   const [userName, setUserName] = useState("");
@@ -17,6 +18,16 @@ function CreateUser() {
     dispatch(updateName(userName));
     navigate("/menu");
   }
+
+  if (user)
+    return (
+      <div>
+        <Button type="primary" to="/menu">
+          Start Ordering, {user}
+        </Button>
+      </div>
+    );
+
   return (
     <form onSubmit={handleSubmit} className="*:mb-6">
       <p className="text-sm text-text-color md:text-base">
